6fec34c40d0a9b2653fa69e73e9ee125f758be35
HedonisticOpportunist <fujitahibaraki@gmail.com>
2022-12-24 22:29:59 +0000

    Add files via upload

--- views/load-article.js
+++ views/load-article.js
@@ -4,19 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
 <head>
     <title>Home Page</title>
      
-    <style type="text/css" media="screen">
-        body {
-            background-color: skyblue;
-            text-decoration-color: white;
-            font-size:1em;
-        }
-    </style>
 </head>
  
 <body>
-    <center>This is the reader's article page.</center>
+    <center>This is the reader's home page.</center>
 </body>
 </html>   
