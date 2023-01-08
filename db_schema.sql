a9b1c71a475dad40072362db57c616b0df6ab6c1
HedonisticOpportunist <fujitahibaraki@gmail.com>
2023-01-08 02:39:44 +0000

    Add files via upload

--- db_schema.sql
+++ db_schema.sql
@@ -48,17 +48,17 @@ CREATE TABLE IF NOT EXISTS comments_db
 
 -- CREATE PUBLISHED ARTICLES --
 INSERT INTO articles_db (title, subtitle, text, date_published, date_modified, number_of_likes)
-VALUES('In the Mountains', 'The Start', 'I do not know where I am.', '11/01/2001', '16/01/2001', 0);
+VALUES('In the Mountains', 'The Start', 'I do not know where I am.', '2010-11-11', '2016-02-13', 0);
 
 INSERT INTO articles_db (title, subtitle, text, date_published, date_modified, number_of_likes)
-VALUES('Up the Mountains', 'Lost', 'I still do not know where I am.', '12/01/2001','14/01/2001', 0);
+VALUES('Up the Mountains', 'Lost', 'I still do not know where I am.', '2009-05-01','2021-03-14', 0);
 
 -- CREATE DRAFT ARTICLES --
 INSERT INTO drafts_db (title, subtitle, text, date_published, date_modified, number_of_likes)
-VALUES('Near the Mountains', 'Where am I?', 'I am lost', '11/01/2003', '11/01/2001', 0);
+VALUES('Near the Mountains', 'Where am I?', 'I am lost', '2003-11-06', '2011-09-11', 0);
 
 INSERT INTO drafts_db (title, subtitle, text, date_published, date_modified, number_of_likes)
-VALUES('Lost in the Mountains', 'Where is my map?', 'I have lost my map.', '12/01/2001','13/01/2001', 0);
+VALUES('Lost in the Mountains', 'Where is my map?', 'I have lost my map.', '2001-03-13','2005-06-10', 0);
 
 -- CREATE CURRENT AUTHOR SETTINGS
 INSERT INTO blog_settings_db (title, subtitle, author)
@@ -66,10 +66,10 @@ VALUES('Lost in the Mountains', 'Tales of a Wandering Samurai', 'Kenshin Himura'
 
 -- CREATE SOME USER COMMENTS --
 INSERT INTO comments_db (text, author, date_published, comment_id)
-VALUES('Awesome', 'Akita', '11/01/2002', 1);
+VALUES('Awesome', 'Akita', '2002-03-29', 1);
 
 INSERT INTO comments_db (text, author, date_published, comment_id)
-VALUES('Still awesome', 'Kaoru', '11/01/2003', 2);
+VALUES('Still awesome', 'Kaoru', '2015-12-05', 2);
 
 COMMIT;
 
